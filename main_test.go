f783f78c3bdd0be3566b89143c2b3969dd9de23d
Aleksandr Britvin <lexbritvin@gmail.com>
2025-05-17 23:20:18 +0200

    WIP #83 TestScript

--- main_test.go
+++ main_test.go
@@ -39,26 +39,10 @@ func TestScriptCommon(t *testing.T) {
 		t.Skip("skipping test in short mode.")
 	}
 	t.Parallel()
-	type testCase struct {
-		name  string
-		files []string
-	}
-	commonDir := "test/testdata/common"
-	tests := []testCase{
-		{"action discovery basic", []string{commonDir + "/discovery_basic.txtar"}},
-		{"action discovery config naming", []string{commonDir + "/discovery_config_naming.txtar"}},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-			testscript.Run(t, testscript.Params{
-				Files:               tt.files,
-				RequireExplicitExec: true,
-				RequireUniqueNames:  true,
-				ContinueOnError:     true,
-			})
-		})
-	}
-
+	testscript.Run(t, testscript.Params{
+		Dir:                 "test/testdata/common",
+		RequireExplicitExec: true,
+		RequireUniqueNames:  true,
+		ContinueOnError:     true,
+	})
 }
